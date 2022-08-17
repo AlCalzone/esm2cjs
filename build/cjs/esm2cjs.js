9b3a350afc338515ecdf02e6b2af260d01cb43fe
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-08-17 12:51:24 +0200

    chore: compile

--- build/cjs/esm2cjs.js
+++ build/cjs/esm2cjs.js
@@ -1,33 +1,36 @@
+"use strict";
 var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
 var __export = (target, all) => {
-  __markAsModule(target);
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
 };
-var __reExport = (target, module2, desc) => {
-  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
-    for (let key of __getOwnPropNames(module2))
-      if (!__hasOwnProp.call(target, key) && key !== "default")
-        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
   }
-  return target;
+  return to;
 };
-var __toModule = (module2) => {
-  return __reExport(__markAsModule(__defProp(module2 != null ? __create(__getProtoOf(module2)) : {}, "default", module2 && module2.__esModule && "default" in module2 ? { get: () => module2.default, enumerable: true } : { value: module2, enumerable: true })), module2);
-};
-__export(exports, {
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var esm2cjs_exports = {};
+__export(esm2cjs_exports, {
   esm2cjs: () => esm2cjs
 });
-var import_path = __toModule(require("path"));
-var import_esbuild = __toModule(require("esbuild"));
-var import_fs_extra = __toModule(require("fs-extra"));
-var import_tiny_glob = __toModule(require("tiny-glob"));
+module.exports = __toCommonJS(esm2cjs_exports);
+var import_path = __toESM(require("path"), 1);
+var import_esbuild = require("esbuild");
+var import_fs_extra = __toESM(require("fs-extra"), 1);
+var import_tiny_glob = __toESM(require("tiny-glob"), 1);
 async function esm2cjs({ inDir, outDir, globs = ["**/*.js"], sourcemap = true, logLevel = "warning", platform = "node", target = "node10", cleanOutDir = false, writePackageJson = true }) {
   if (cleanOutDir)
     await import_fs_extra.default.emptyDir(outDir);
